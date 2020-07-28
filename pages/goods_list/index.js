c9bdaa6491d95a4971585e525201a6466a765dab
LiuQ-Q <545784977@qq.com>
2020-07-28 23:45:18 +0800

    pages: goods_list

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,66 +1,95 @@
 // pages/goods_list/index.js
+import { request } from "../../request/index";
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    tabs: [
+      {
+        id: 0,
+        value: "综合",
+        isActive: true
+      },
+      {
+        id: 1,
+        value: "销量",
+        isActive: false
+      },
+      {
+        id: 0,
+        value: "价格",
+        isActive: false
+      }
+    ],
+    goodList: []
   },
+
+  QueryParams: {
+    query: "",
+    cid: "",
+    pagenum: 1,
+    pagesize: 10
+  },
+
+  totalPages: 1,
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.QueryParams.cid = options.cid;
+    this.getGoodList();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  async getGoodList() {
+    const res = await request({url:"/goods/search", data: this.QueryParams});
+    const total = res.total;
 
+    this.totalPages = Math.ceil(total / this.QueryParams.pagesize);
+    this.setData({
+      goodList: [...this.data.goodList, ...res.goods]
+    });
+
+    // 关闭下拉刷新框
+    wx.stopPullDownRefresh();
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  tabsItemChange(e) {
+    const index = e.detail;
+    let { tabs } = this.data;
+    
+    tabs.forEach((tab, tabIndex) => {
+      tabIndex === index ? tab.isActive = true : tab.isActive = false;
+    });
 
+    this.setData({
+      tabs
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  // 触底加载
+  onReachBottom() {
+    if (this.QueryParams.pagenum >= this.totalPages) {
+      wx.showToast({
+        title: '全部加载完成了!'
+      });
+      
+    } else {
+      // console.log('you');
+      this.QueryParams.pagenum++;
+      this.getGoodList();
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 下拉刷新
+  onPullDownRefresh() {
+    this.setData({
+      goodList: []
+    });
+    this.QueryParams.pagenum = 1;
+    this.getGoodList();
   }
 })
